8f9d8992ff99a3048be71c2746d5b55670f38e8e
ouidane <ouidanezakaria@gmail.com>
2025-07-13 20:43:15 +0100

    feat: add allDepartments array and update categories in menuData

--- frontend/src/components/Header/menuData.ts
+++ frontend/src/components/Header/menuData.ts
@@ -18,145 +18,191 @@ export const menuData: Menu[] = [
     title: "Contact",
     newTab: false,
     path: "/contact",
-  },
-  // {
-  //   id: 6,
-  //   title: "pages",
-  //   newTab: false,
-  //   path: "/",
-  //   submenu: [
-  //     {
-  //       id: 61,
-  //       title: "Shop With Sidebar",
-  //       newTab: false,
-  //       path: "/shop-with-sidebar",
-  //     },
-  //     {
-  //       id: 62,
-  //       title: "Shop Without Sidebar",
-  //       newTab: false,
-  //       path: "/shop-without-sidebar",
-  //     },
-  //     {
-  //       id: 64,
-  //       title: "Checkout",
-  //       newTab: false,
-  //       path: "/checkout",
-  //     },
-  //     {
-  //       id: 65,
-  //       title: "Cart",
-  //       newTab: false,
-  //       path: "/cart",
-  //     },
-  //     {
-  //       id: 66,
-  //       title: "Wishlist",
-  //       newTab: false,
-  //       path: "/wishlist",
-  //     },
-  //     {
-  //       id: 67,
-  //       title: "Sign in",
-  //       newTab: false,
-  //       path: "/signin",
-  //     },
-  //     {
-  //       id: 68,
-  //       title: "Sign up",
-  //       newTab: false,
-  //       path: "/signup",
-  //     },
-  //     {
-  //       id: 69,
-  //       title: "My Account",
-  //       newTab: false,
-  //       path: "/my-account",
-  //     },
-  //     {
-  //       id: 70,
-  //       title: "Contact",
-  //       newTab: false,
-  //       path: "/contact",
-  //     },
-  //     {
-  //       id: 62,
-  //       title: "Error",
-  //       newTab: false,
-  //       path: "/error",
-  //     },
-  //     {
-  //       id: 63,
-  //       title: "Mail Success",
-  //       newTab: false,
-  //       path: "/mail-success",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 7,
-  //   title: "blogs",
-  //   newTab: false,
-  //   path: "/",
-  //   submenu: [
-  //     {
-  //       id: 71,
-  //       title: "Blog Grid with sidebar",
-  //       newTab: false,
-  //       path: "/blogs/blog-grid-with-sidebar",
-  //     },
-  //     {
-  //       id: 72,
-  //       title: "Blog Grid",
-  //       newTab: false,
-  //       path: "/blogs/blog-grid",
-  //     },
-  //     {
-  //       id: 73,
-  //       title: "Blog details with sidebar",
-  //       newTab: false,
-  //       path: "/blogs/blog-details-with-sidebar",
-  //     },
-  //     {
-  //       id: 74,
-  //       title: "Blog details",
-  //       newTab: false,
-  //       path: "/blogs/blog-details",
-  //     },
-  //   ],
-  // },
+  }
 ];
 
 export const categories = [
   {
     _id: "65a0a8e63e8480d8cc796b34",
     name: "camera & photo",
-    description: "Equipment and accessories related to photography.",
   },
   {
     _id: "65a0a8e63e8480d8cc796b40",
     name: "smartphones & tablets",
-    description:
-      "Mobile devices and accessories for communication and productivity.",
   },
   {
     _id: "65a0a8e63e8480d8cc796b5c",
     name: "laptops & computers",
-    description: "Computing devices and related accessories.",
   },
   {
     _id: "65a0a8e73e8480d8cc796b6e",
     name: "audio",
-    description: "Sound-related devices and accessories.",
   },
   {
     _id: "65a0a8e73e8480d8cc796b90",
     name: "office electronics",
-    description: "Devices and equipment commonly used in office environments.",
   },
   {
     _id: "65a0a8e53e8480d8cc796b26",
     name: "television & video",
-    description: "Devices and equipment related to visual entertainment.",
+  },
+];
+
+export const allDepartments = [
+  {
+    _id: "65a0a8e53e8480d8cc796b26",
+    name: "television & video",
+    subCategories: [
+      {
+        _id: "65a0a8e53e8480d8cc796b28",
+        name: "televisions",
+      },
+      {
+        _id: "65a0a8e53e8480d8cc796b2a",
+        name: "streaming media player",
+      },
+      {
+        _id: "65a0a8e53e8480d8cc796b29",
+        name: "home theater systems",
+      },
+    ],
+  },
+  {
+    _id: "65a0a8e63e8480d8cc796b34",
+    name: "camera & photo",
+    subCategories: [
+      {
+        _id: "65a0a8e63e8480d8cc796b36",
+        name: "wearable & action cameras",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b37",
+        name: "digital cameras",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b3a",
+        name: "camera lenses",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b38",
+        name: "camcorders",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b39",
+        name: "photo printers",
+      },
+      {
+        _id: "677f214e7d15a3f322d46da0",
+        name: "security cameras",
+      },
+    ],
+  },
+  {
+    _id: "65a0a8e63e8480d8cc796b40",
+    name: "smartphones & tablets",
+    subCategories: [
+      {
+        _id: "65a0a8e63e8480d8cc796b42",
+        name: "smartphones",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b43",
+        name: "tablets",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b45",
+        name: "smartwatches",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b46",
+        name: "phone cases",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b47",
+        name: "chargers",
+      },
+    ],
+  },
+  {
+    _id: "65a0a8e63e8480d8cc796b5c",
+    name: "laptops & computers",
+    subCategories: [
+      {
+        _id: "65a0a8e63e8480d8cc796b63",
+        name: "graphics cards",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b61",
+        name: "desktops",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b5e",
+        name: "gaming laptops",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b62",
+        name: "external hard drives",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b60",
+        name: "macbooks",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b5f",
+        name: "traditional laptops",
+      },
+      {
+        _id: "65a0a8e63e8480d8cc796b64",
+        name: "mouse",
+      },
+    ],
+  },
+  {
+    _id: "65a0a8e73e8480d8cc796b6e",
+    name: "audio",
+    subCategories: [
+      {
+        _id: "65a0a8e73e8480d8cc796b73",
+        name: "receivers",
+      },
+      {
+        _id: "65a0a8e73e8480d8cc796b70",
+        name: "headphones",
+      },
+      {
+        _id: "65a0a8e73e8480d8cc796b72",
+        name: "audio & video accessories",
+      },
+      {
+        _id: "65a0a8e73e8480d8cc796b71",
+        name: "speakers",
+      },
+    ],
+  },
+  {
+    _id: "65a0a8e73e8480d8cc796b90",
+    name: "office electronics",
+    subCategories: [
+      {
+        _id: "65a0a8e83e8480d8cc796b92",
+        name: "printers",
+      },
+      {
+        _id: "65a0a8e83e8480d8cc796b95",
+        name: "office phones",
+      },
+      {
+        _id: "65a0a8e83e8480d8cc796b93",
+        name: "scanners",
+      },
+      {
+        _id: "65a0a8e83e8480d8cc796b94",
+        name: "fax machines",
+      },
+      {
+        _id: "65a0a8e83e8480d8cc796b96",
+        name: "shredders",
+      },
+    ],
   },
 ];
