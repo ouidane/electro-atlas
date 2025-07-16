4b2119d043b15f4d2d8f82213d83b7986bf472f5
ouidane <ouidanezakaria@gmail.com>
2025-07-16 13:31:20 +0100

    feat(types): add types for product filters and filter response

---
+++ frontend/src/types/filters.ts
@@ -0,0 +1,31 @@
+
+export type CategoryWithSubcategories = {
+  category: {
+    _id: string;
+    name: string;
+  };
+  subcategories: Array<{
+    _id: string;
+    name: string;
+  }>;
+};
+
+export type PriceRange = {
+  highestPrice: number;
+  lowestPrice: number;
+};
+
+export type Specification = {
+  key: string;
+  values: string[];
+};
+
+export type Filters = {
+  categories: CategoryWithSubcategories[];
+  priceRange: PriceRange[];
+  specifications: Specification[];
+};
+
+export type FilterResponse = {
+  data: Filters;
+}
