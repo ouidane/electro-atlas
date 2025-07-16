41eaf387e530ad9a1e71862508f8fd519fddd35a
ouidane <ouidanezakaria@gmail.com>
2025-07-16 13:10:07 +0100

    feat: use products mock data for promo banners and update styles

--- frontend/src/components/Home/PromoBanner/index.tsx
+++ frontend/src/components/Home/PromoBanner/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Image from "next/image";
+import { products } from "../muck";
+
+const [s25Ultra, iphone16e, watchUltra] = products.slice(1, 4);
 
 const PromoBanner = () => {
   return (
@@ -9,20 +12,17 @@ const PromoBanner = () => {
         <div className="relative z-1 overflow-hidden rounded-lg bg-[#F5F5F7] py-12.5 lg:py-17.5 xl:py-22.5 px-4 sm:px-7.5 lg:px-14 xl:px-19 mb-7.5">
           <div className="max-w-[550px] w-full">
             <span className="block font-medium text-xl text-dark mb-3">
-              Apple iPhone 14 Plus
+              {s25Ultra.name}
             </span>
 
             <h2 className="font-bold text-xl lg:text-heading-4 xl:text-heading-3 text-dark mb-5">
-              UP TO 30% OFF
+              UP TO {s25Ultra.variant.discountPercent}% OFF
             </h2>
 
-            <p>
-              iPhone 14 has the same superspeedy chip that’s in iPhone 13 Pro,
-              A15 Bionic, with a 5‑core GPU, powers all the latest features.
-            </p>
+            <p>{s25Ultra.description}</p>
 
             <a
-              href="#"
+              href={`#/products/${s25Ultra.id}`}
               className="inline-flex font-medium text-custom-sm text-white bg-blue py-[11px] px-9.5 rounded-md ease-out duration-200 hover:bg-blue-dark mt-7.5"
             >
               Buy Now
@@ -30,9 +30,9 @@ const PromoBanner = () => {
           </div>
 
           <Image
-            src="/images/promo/promo-01.png"
-            alt="promo img"
-            className="absolute bottom-0 right-4 lg:right-26 -z-1"
+            src={s25Ultra.image.large}
+            alt={s25Ultra.name}
+            className="absolute bottom-4 right-4 lg:right-26 -z-1"
             width={274}
             height={350}
           />
@@ -42,8 +42,8 @@ const PromoBanner = () => {
           {/* <!-- promo banner small --> */}
           <div className="relative z-1 overflow-hidden rounded-lg bg-[#DBF4F3] py-10 xl:py-16 px-4 sm:px-7.5 xl:px-10">
             <Image
-              src="/images/promo/promo-02.png"
-              alt="promo img"
+              src={iphone16e.image.large}
+              alt={iphone16e.name}
               className="absolute top-1/2 -translate-y-1/2 left-3 sm:left-10 -z-1"
               width={241}
               height={241}
@@ -51,19 +51,19 @@ const PromoBanner = () => {
 
             <div className="text-right">
               <span className="block text-lg text-dark mb-1.5">
-                Foldable Motorised Treadmill
+                {iphone16e.name}
               </span>
 
               <h2 className="font-bold text-xl lg:text-heading-4 text-dark mb-2.5">
-                Workout At Home
+                {iphone16e.modelName}
               </h2>
 
               <p className="font-semibold text-custom-1 text-teal">
-                Flat 20% off
+                Flat {iphone16e.variant.discountPercent}% off
               </p>
 
               <a
-                href="#"
+                href={`#/products/${iphone16e.id}`}
                 className="inline-flex font-medium text-custom-sm text-white bg-teal py-2.5 px-8.5 rounded-md ease-out duration-200 hover:bg-teal-dark mt-9"
               >
                 Grab Now
@@ -72,10 +72,10 @@ const PromoBanner = () => {
           </div>
 
           {/* <!-- promo banner small --> */}
-          <div className="relative z-1 overflow-hidden rounded-lg bg-[#FFECE1] py-10 xl:py-16 px-4 sm:px-7.5 xl:px-10">
+          <div className="relative z-1 overflow-hidden rounded-lg bg-[#E1E8FF] py-10 xl:py-16 px-4 sm:px-7.5 xl:px-10">
             <Image
-              src="/images/promo/promo-03.png"
-              alt="promo img"
+              src={watchUltra.image.large}
+              alt={watchUltra.name}
               className="absolute top-1/2 -translate-y-1/2 right-3 sm:right-8.5 -z-1"
               width={200}
               height={200}
@@ -83,21 +83,20 @@ const PromoBanner = () => {
 
             <div>
               <span className="block text-lg text-dark mb-1.5">
-                Apple Watch Ultra
+                {watchUltra.name}
               </span>
 
               <h2 className="font-bold text-xl lg:text-heading-4 text-dark mb-2.5">
-                Up to <span className="text-orange">40%</span> off
+                Up to <span className="text-blue">{watchUltra.variant.discountPercent}%</span> off
               </h2>
 
               <p className="max-w-[285px] text-custom-sm">
-                The aerospace-grade titanium case strikes the perfect balance of
-                everything.
+                {watchUltra.description}
               </p>
 
               <a
-                href="#"
-                className="inline-flex font-medium text-custom-sm text-white bg-orange py-2.5 px-8.5 rounded-md ease-out duration-200 hover:bg-orange-dark mt-7.5"
+                href={`#/products/${watchUltra.id}`}
+                className="inline-flex font-medium text-custom-sm text-white bg-blue py-2.5 px-8.5 rounded-md ease-out duration-200 hover:bg-blue-dark mt-7.5"
               >
                 Buy Now
               </a>
