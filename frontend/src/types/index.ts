a49da757226c37365a1387e669d94739a81ac384
ouidane <ouidanezakaria@gmail.com>
2025-07-16 13:31:20 +0100

    chore(types): update index to export new and existing types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,6 @@
+export * from "./product";
+export * from "./category";
+export * from "./Menu";
+export * from "./testimonial";
+export * from "./filters";
+export * from "../components/Home/muck"; 
