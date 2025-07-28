f1975acc946a8f3692aa11f8f123ecf4e73a7ee7
ouidane <ouidanezakaria@gmail.com>
2025-07-28 22:26:56 +0100

    refactor(discoverController): simplify query param parsing using parsedQuery
    Use `req.parsedQuery` with typed schemas instead of manually extracting query params for bestOffers, bestSeller, and recommendedProducts routes.

--- backend/src/controllers/discoverController.ts
+++ backend/src/controllers/discoverController.ts
@@ -1,36 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import { DiscoverService } from "../services/discoverService";
 import { QueryFiltersType } from "../middlewares/validateProduct";
+import {
+  BestOffersType,
+  BestSellingType,
+  RecommendationType,
+} from "../middlewares";
 
 export const discoverService = new DiscoverService();
 
 class DiscoverController {
   // Get best offers
   async bestOffers(req: Request, res: Response, next: NextFunction) {
-    const {
-      categoryId,
-      subCategoryId,
-    }: { categoryId?: string; subCategoryId?: string } = req.query;
-
-    const data = await discoverService.getBestOffers({
-      categoryId,
-      subCategoryId,
-    });
+    const queryParams = req.parsedQuery as BestOffersType;
+    const data = await discoverService.getBestOffers(queryParams);
 
     res.status(200).json({ data });
   }
 
   // Get best sellers
   async bestSeller(req: Request, res: Response, next: NextFunction) {
-    const {
-      categoryId,
-      subCategoryId,
-    }: { categoryId?: string; subCategoryId?: string } = req.query;
-
-    const data = await discoverService.getBestSellers({
-      categoryId,
-      subCategoryId,
-    });
+    const queryParams = req.parsedQuery as BestSellingType;
+    const data = await discoverService.getBestSellers(queryParams);
 
     res.status(200).json({ data });
   }
@@ -38,28 +29,15 @@ class DiscoverController {
   // Products filter
   async productsFilter(req: Request, res: Response, next: NextFunction) {
     const queryfilters = req.parsedQuery as QueryFiltersType;
-
     const data = await discoverService.getProductsFilter(queryfilters);
+
     res.status(200).json({ data });
   }
 
   // Get recommended products
   async recommendedProducts(req: Request, res: Response, next: NextFunction) {
-    const {
-      categoryId,
-      subCategoryId,
-      excludeProductId,
-    }: {
-      categoryId?: string;
-      subCategoryId?: string;
-      excludeProductId?: string;
-    } = req.query;
-
-    const data = await discoverService.getRecommendedProducts({
-      categoryId,
-      subCategoryId,
-      excludeProductId,
-    });
+    const queryParams = req.parsedQuery as RecommendationType;
+    const data = await discoverService.getRecommendedProducts(queryParams);
 
     res.status(200).json({ data });
   }
