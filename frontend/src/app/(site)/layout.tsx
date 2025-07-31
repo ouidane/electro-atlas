0ebd6dd658652b20b52fcb4f74cc04243b338caa
ouidane <ouidanezakaria@gmail.com>
2025-07-31 03:06:15 +0100

    feat: Add AuthProvider to root layout for global context

--- frontend/src/app/(site)/layout.tsx
+++ frontend/src/app/(site)/layout.tsx
@@ -15,6 +15,7 @@ import PreviewSliderModal from "@/components/Common/PreviewSlider";
 
 import ScrollToTop from "@/components/Common/ScrollToTop";
 import PreLoader from "@/components/Common/PreLoader";
+import AuthProvider from "@/app/context/AuthProvider";
 
 export default function RootLayout({
   children,
@@ -35,18 +36,20 @@ export default function RootLayout({
         ) : (
           <>
             <ReduxProvider>
-              <CartModalProvider>
-                <ModalProvider>
-                  <PreviewSliderProvider>
-                    <Header />
-                    {children}
+              <AuthProvider>
+                <CartModalProvider>
+                  <ModalProvider>
+                    <PreviewSliderProvider>
+                      <Header />
+                      {children}
 
-                    <QuickViewModal />
-                    <CartSidebarModal />
-                    <PreviewSliderModal />
-                  </PreviewSliderProvider>
-                </ModalProvider>
-              </CartModalProvider>
+                      <QuickViewModal />
+                      <CartSidebarModal />
+                      <PreviewSliderModal />
+                    </PreviewSliderProvider>
+                  </ModalProvider>
+                </CartModalProvider>
+              </AuthProvider>
             </ReduxProvider>
             <ScrollToTop />
             <Footer />
