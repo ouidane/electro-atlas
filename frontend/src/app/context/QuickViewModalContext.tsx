29c693a6be520a11dbacd00c9e28279452fcc4ac
ouidane <ouidanezakaria@gmail.com>
2025-07-31 02:15:25 +0100

    fix(context): add missing semicolon in QuickViewModalContext client directive

--- frontend/src/app/context/QuickViewModalContext.tsx
+++ frontend/src/app/context/QuickViewModalContext.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { createContext, useContext, useState } from "react";
 
 interface ModalContextType {
@@ -33,4 +33,4 @@ export const ModalProvider = ({ children }) => {
       {children}
     </ModalContext.Provider>
   );
-}; 
+};
