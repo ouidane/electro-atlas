7c14b1712dbf87adb2451fa2327bbf22be20379a
ouidane <ouidanezakaria@gmail.com>
2025-07-31 02:05:53 +0100

    refactor(api): use auth.accessToken instead of authReducer.accessToken in baseQuery headers

--- frontend/src/redux/features/api.ts
+++ frontend/src/redux/features/api.ts
@@ -1,12 +1,11 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-import type { FetchArgs, FetchBaseQueryError } from "@reduxjs/toolkit/query";
 
 export const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_SERVER_URL,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
-    const token = (getState() as RootState).authReducer.accessToken;
+    const token = (getState() as RootState).auth.accessToken;
     if (token) headers.set("Authorization", `Bearer ${token}`);
     return headers;
   },
